fb837fc3134fc88576c92bcbd745fe1b78411f2f
Luke Mortimer <lbmortimer1998@gmail.com>
2019-01-17 12:23:49 +0000

    directory change

--- ggfirmware/ggfirmware.ino
+++ ggfirmware/ggfirmware.ino
@@ -14,29 +14,32 @@ int lastPin = -1;
 // r, g, b
 int ledPins[] = {9, 10, 11};
 
+char data;
+int i;
+
 void setup() {
 
     // Start the i2c comms
     Wire.begin(address);
 
     // Set all the pins to output
-    for (int i=0; i<6; i++) {pinMode(handPins[i], OUTPUT);}
+    for (i=0; i<6; i++) {pinMode(handPins[i], OUTPUT);}
 
 }
 
 void loop() {
 
     // If there's any data, process it
-    while (Wire.available()) {
+    while (Wire.available() >= 0) {
 
         // Get the byte from i2c
-        int data = Wire.read();
+        data = Wire.read();
 
         // Set a certain pins to be on, whilst turning the last pin off
-        if (data >=1 and data <=6) {
+        if (int(data) >= 1 && int(data) <= 6) {
 
             if (lastPin >= 0) {digitalWrite(lastPin, LOW);}
-            lastPin = handPins[data1-1];
+            lastPin = handPins[int(data)-1];
             digitalWrite(lastPin, HIGH);
 
         }
@@ -49,6 +52,24 @@ void loop() {
 
         // LED to blue
         else if (data == 'b') {led(0, 0, 255);}
+
+        // LED to yellow
+        else if (data == 'y') {led(255, 255, 0);}
+
+        // LED to cyan
+        else if (data == 'c') {led(0, 255, 255);}
+
+        // LED to magenta
+        else if (data == 'm') {led(255, 0, 255);}
+
+        // LED to white
+        else if (data == 'w') {led(255, 255, 255);}
+
+        // LED to orange
+        else if (data == 'o') {led(255, 165, 0);}
+
+        // LED to black
+        else if (data == 'l') {led(0, 0, 0);}
 
     }
 
